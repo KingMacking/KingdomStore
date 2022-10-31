2fa1b6610dae3523a172a3cf61834faf4037250d
King Macking <matiasatzori@gmail.com>
2022-10-31 14:32:52 -0300

    Generar orden en proceso, agregadas librerias de validacion de datos de formularios, agregada toastify

--- src/components/Pages/CartPage/OrderForm.jsx
+++ src/components/Pages/CartPage/OrderForm.jsx
@@ -1,26 +1,33 @@
 import { addDoc, collection, documentId, getDocs, getFirestore, query, where, writeBatch } from 'firebase/firestore'
 import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Ring } from '@uiball/loaders'
+import { toast } from 'react-toastify';
+import * as yup from "yup";
 
-export const OrderForm = ({cartList, emptyCart, cartTotalPrice, cartQuant}) => {
-    const[dataForm, setDataForm] = useState({
-        name: '',
-        phone: '',
-        email: ''
+import './OrderForm.scss'
+
+const generateOrderSchema = yup.object()
+    .shape({
+        name: yup.string().required("Debes ingresar tu nombre"),
+        phone: yup.number("Debes ingresar un numero valido").positive("No se pueden usar numeros negativos").integer("No se pueden utilizar numeros con coma").required("Debes ingresar un numero valido"),
+        email: yup.string().email("Debes ingresar un email valido").required("Debes ingresar un email valido"),
+        confirmEmail: yup.string().oneOf([yup.ref('email'), null], "Los emails deben coincidir").required("Los emails deben coincidir")
     })
 
-    const handleInputChange = (e) => {        
-        setDataForm({
-            ...dataForm,
-            [e.target.name]: e.target.value
-        })
-    }
+export const OrderForm = ({cartList, emptyCart, cartTotalPrice, cartQuant}) => {
+    const[dataForm, setDataForm] = useState({})
+
+    const {register, handleSubmit, formState: { errors }} = useForm( {resolver: yupResolver(generateOrderSchema)});
+
+    
 
     const db = getFirestore()
     const orders = collection(db, 'orders')
-    const queryCollection = collection(db, 'items')
+    const queryCollection = collection(db, 'games')
 
-    const generateOrder = async (e) => {
-        e.preventDefault()
+    const generateOrder = async () => {
         const order = {}
 
         order.buyer = {
@@ -39,32 +46,84 @@ export const OrderForm = ({cartList, emptyCart, cartTotalPrice, cartQuant}) => {
 
         order.date = new Date().toLocaleDateString()
 
-        addDoc(orders, order)
-        .finally(emptyCart)
+        
 
-        const queryUpdateStock = await query(queryCollection, where(documentId(), 'in', cartList.map(item => item.id)))
+        const queryUpdateStock = query(queryCollection, where(documentId(), 'in', cartList.map(item => item.id)))
 
         const batch = writeBatch(db)
 
         await getDocs(queryUpdateStock)
-        .then(resp => resp.docs.forEach(res => batch.update({
+        .then(resp => resp.docs.forEach(res => batch.update(res.ref, {
             stock: (res.data().stock - cartList.find(item => item.id === res.id).quant)
         })))
-
         batch.commit()
+
+        await addDoc(orders, order)
+        .finally(emptyCart)
+
+        toast.success('Compra finalizada', {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: false,
+            progress: undefined,
+            theme: "dark",
+        })
     }
-    
-    
 
     return (
         <div>
-            <form onSubmit={generateOrder}>
-                <input type="text" name="name" placeholder="Nombre" value={dataForm.name} onChange={handleInputChange}/>
-                <input type="tel" name="phone" placeholder="Telefono de contacto" value={dataForm.phone} onChange={handleInputChange}/>
-                <input type="email" name="email" placeholder="Email" value={dataForm.email} onChange={handleInputChange}/>
-                <input type="email" name="email" placeholder="Email" value={dataForm.email} onChange={handleInputChange}/>
-                <button className="btn cart-btn" type="submit">Finalizar compra</button>
-            </form>
+            <h2 className='form-title'>Formulario de compra</h2>
+            <div className='form-detail'>
+                <form className='form-buy' onSubmit={handleSubmit((data) => {
+                    setDataForm({...data})
+                    generateOrder()
+                    toast.info('Generando orden', {
+                        position: "top-right",
+                        autoClose: 2000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: false,
+                        progress: undefined,
+                        theme: "dark",
+                        icon: <Ring size={40} lineWeight={5} speed={2}  color="white" />
+                    })
+                })} >
+                    <div>
+                        <input className='form-buy-input' type="text" {...register("name")} placeholder="Nombre completo"/>
+                        {errors.name && <p className='form-error'>{errors.name.message}</p>}
+                    </div>
+                    <div>
+                        <input className='form-buy-input' type="tel" {...register("phone")} placeholder="Telefono de contacto"/>
+                        {errors.phone && <p className='form-error'>{errors.phone.message}</p>}
+                    </div>
+                    <div>
+                        <input className='form-buy-input' type="email" {...register("email")} placeholder="Email"/>
+                        {errors.email && <p className='form-error'>{errors.email.message}</p>}
+                    </div>
+                    <div>
+                        <input className='form-buy-input' type="email" {...register("confirmEmail")} placeholder="Repetir email"/>
+                        {errors.confirmEmail && <p className='form-error'>{errors.confirmEmail.message}</p>}
+                    </div>
+                    <button className="btn cart-btn" type="submit">Finalizar compra</button>
+                </form>
+                <div className="cart-info">
+                    <h3 className="cart-info-title">Subtotal</h3>
+                    <div className="cart-info-detail">
+                        <p className="cart-info-data">Cantidad: {cartQuant} Items</p>
+                        {
+                            <>
+                                <p className="cart-info-data">Sumatoria: ${cartTotalPrice}</p>
+                                {cartQuant >= 15 && <p className="cart-info-offer">Oferta por cantidad: 10% (15+)</p> || cartQuant >= 10 && <p className="cart-info-offer">Oferta por cantidad: 5% (10+)</p>}
+                                {cartQuant >= 15 && <h4 className="cart-info-total">Total: ${cartTotalPrice - (cartTotalPrice*0.1)}</h4> || cartQuant >= 10 && <h4 className="cart-info-total">Total: ${cartTotalPrice - (cartTotalPrice*0.05)}</h4> || <h4 className="cart-info-total">Total: ${cartTotalPrice}</h4>}
+                            </>
+                        }
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
